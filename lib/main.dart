0c4eca81c348137974f6e3f1bf5020a09b28be28
Ha Cong Huong <huonghacong2005@gmail.com>
2025-11-04 19:55:28 +0700

    feat: thêm danh sách quốc gia

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,15 @@ class TrangChinh extends StatelessWidget {
               label: const Text(
                 'Món ăn nổi tiếng',
               ),
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) =>
+                        const QuocGia(),
+                  ),
+                );
+              },
             ),
             const SizedBox(height: 20),
             ElevatedButton.icon(
@@ -61,6 +69,72 @@ class TrangChinh extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class QuocGia extends StatelessWidget {
+  const QuocGia({super.key});
+
+  final List<Map<String, String>> quocgia =
+      const [
+        {
+          'ten': 'Nhật Bản',
+          'anh': 'imgs/nhatban.png',
+        },
+        {
+          'ten': 'Việt Nam',
+          'anh': 'imgs/vietnam.jpeg',
+        },
+        {'ten': 'Ý', 'anh': 'imgs/italy.png'},
+        {
+          'ten': 'Mexico',
+          'anh': 'imgs/mexico.png',
+        },
+      ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chọn quốc gia'),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(12),
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 12,
+            ),
+        itemCount: quocgia.length,
+        itemBuilder: (context, index) {
+          var qg = quocgia[index];
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(
+              16,
+            ),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Image.asset(
+                  qg['anh']!,
+                  fit: BoxFit.cover,
+                ),
+                Container(
+                  color: Colors.black45,
+                  child: Text(
+                    qg['ten']!,
+                    style: const TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
